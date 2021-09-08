b75dd213ac6e1eaff6875eb133f2bf0052ada612
ADHAO <ADHAO@192.168.1.7>
2021-09-08 13:01:34 +0700

    Second Commit

---
+++ Learnjava/src/Chuvi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Chuvi {
+	
+	
+	 
+	    public static double tinhchuvi(double r){
+	        return r * 2 * 3.14;
+	    }
+	    public static void main(String[] args) {
+	        double r;
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Nhập bán kính hình tròn: ");
+	        r = scanner.nextDouble();
+	   
+	        System.out.println("Chu vi hình tròn là: "+ tinhchuvi(r));
+	   
+	   
+	    }
+	}
