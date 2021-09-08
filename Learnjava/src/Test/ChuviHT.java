dacf7afbf307c412fa5fb7268f1061a576b63be2
ADHAO <ADHAO@192.168.1.7>
2021-09-08 15:42:22 +0700

    Test2

--- Learnjava/src/Test/ChuviHT.java
+++ Learnjava/src/Test/ChuviHT.java
@@ -2,16 +2,16 @@ package Test;
 
 import java.util.Scanner;
 public class ChuviHT {
-	    public static double tinhChuVi(double r){
+	    public static double ChuVi(double r){
 	        return r * 2 * 3.14;
 	    }
 	 
 	public static void main(String[] args) {
 		 	double r;
 	        Scanner scanner = new Scanner(System.in);
-	        System.out.println("Nhập bán kính đường tròn: ");
+	        System.out.println("Nhập bán kính đường tròn:  ");
 	        r = scanner.nextDouble();
-	        System.out.println("Chu vi hình tròn là: "+ tinhChuVi(r));
+	        System.out.println("Chu vi hình tròn là: "+ ChuVi(r));
 	}
 
 }
