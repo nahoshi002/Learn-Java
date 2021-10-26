0b26700ec05ae6d4a401eb9d594058ef51b45477
ADHAO <ADHAO@192.168.1.6>
2021-10-26 20:40:51 +0700

    8th commit

--- Learnjava/src/Test6/B6_FindString.java
+++ Learnjava/src/Test6/B6_FindString.java
@@ -5,18 +5,32 @@ import java.util.Scanner;
 public class B6_FindString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Nhập vào chuỗi của bạn: ");
-		Scanner scanner = new Scanner(System.in);
-		String str = scanner.nextLine();
-		
-		System.out.print("Nhập vào chuỗi của bạn: ");
-		String k = scanner.nextLine();
-		int dem=0;
-			if(str.contains(k)) {
-				dem+=1;
-			}
-		System.out.println("counter=" + dem);
+	    String chuoi;
+	    int count = 0;
+	    Scanner scanner = new Scanner(System.in);
+	         
+	    System.out.print("Nhập vào chuỗi bất kỳ: ");
+	    chuoi = scanner.nextLine();
+	    System.out.print("Nhập vào kí tự cần tìm: " );
+	    char kyTu = ' ';
+        try
+        {
+        	kyTu = (char) System.in.read();
+        }
+        catch(Exception e)
+        {
+              System.out.println("Nhập lỗi!");
+        }
+	    // duyệt từ đầu đến cuối chuỗi
+	    for (int i = 0; i < chuoi.length(); i++) {
+	        // Nếu ký tự tại vị trí thứ i bằng 'a' thì tăng count lên 1
+	        if (chuoi.charAt(i) == kyTu) {
+	        	System.out.println("Vị trí "+i+" xuất hiện kí tự "+ kyTu);
+	            count++;
+	        }
+	    }
+	         
+	    System.out.println("Số lần xuất hiện của ký tự " + kyTu +
+	        " trong chuỗi " + chuoi + " = " + count);
 	}
-
 }
